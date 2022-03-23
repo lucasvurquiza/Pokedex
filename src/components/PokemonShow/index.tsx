56955c1f087eb3ea9b7753ed3767da2593177ff9
Lucas Vieira Urquiza <urquizaxd@gmail.com>
2022-03-23 16:20:58 -0300

    feat: Adicionando loadMore por offset e limit
    - Foi feito uma alteração nos types, podendo vir alguns valores como undefined
    - Foi adicionado os valores do loadMore dentro do array antigo
    - Foi utilizado o memo para tornar a experiência e usabilidade melhor
    - Foi criado o component de Loading
    - Foi adicionado uma nova rota no axios que é a do offsetLimit

--- src/components/PokemonShow/index.tsx
+++ src/components/PokemonShow/index.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import Pokemon from 'api/Pokemon';
-import React from 'react';
+import React, {memo} from 'react';
 import {Image, Text, View} from 'react-native';
-import {TItem} from 'utils/types';
+import {TResults} from 'utils/types';
 import {POKE_API} from 'utils/urls';
 import {firstLetterUpper} from 'utils/utils';
 
-export const PokemonShow = (props: TItem) => {
-  const {name, url} = props.item;
+const PokemonShow = (props: TResults) => {
+  const {name, url} = props;
   const pokemonId = url.replace(`${POKE_API}pokemon/`, '').replace('/', '');
 
   return (
@@ -29,3 +29,5 @@ export const PokemonShow = (props: TItem) => {
     </View>
   );
 };
+
+export default memo(PokemonShow);
--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -3,34 +3,94 @@ import React, {useEffect, useState} from 'react';
 import {FlatList, View} from 'react-native';
 import Pokemon from 'api/Pokemon';
 import {TAllPokemons} from 'utils/types';
-import {PokemonShow} from 'components/PokemonShow';
+import PokemonShow from 'components/PokemonShow';
+import {Loading} from 'components/Loading';
+import {POKE_API} from 'utils/urls';
 
 const App = () => {
-  const [pokemons, setPokemons] = useState<TAllPokemons>();
+  const [pokemons, setPokemons] = useState<TAllPokemons>({});
+  const [pokemonOffset, setPokemonOffset] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   useEffect(() => {
     const fetchAllPokemons = async () => {
+      setLoading(true);
       const response: TAllPokemons = await Pokemon.getAllPokemons();
       setPokemons(response);
+      const offset = response?.next?.replace(`${POKE_API}pokemon`, '');
+      setPokemonOffset(offset || '');
+      setLoading(false);
     };
     fetchAllPokemons();
   }, []);
 
+  const loadMoreOnEnd = async () => {
+    setIsRefreshing(true);
+    const response: TAllPokemons = await Pokemon.getLoadMorePokemons(
+      pokemonOffset,
+    );
+    setPokemons(prevState => ({
+      count: response.count,
+      next: response.next,
+      previous: response.previous,
+      results: prevState?.results?.concat(response?.results || []),
+    }));
+    const offset = response?.next?.replace(`${POKE_API}pokemon`, '');
+    setPokemonOffset(offset || '');
+    setIsRefreshing(false);
+  };
+
+  const renderFooter = () => {
+    if (!isRefreshing) {
+      return null;
+    }
+    return (
+      <View style={{marginTop: 10}}>
+        <Loading />
+      </View>
+    );
+  };
+
+  const renderItem = ({item}: any) => (
+    <PokemonShow name={item?.name} url={item?.url} />
+  );
+
   return (
-    <View>
-      <FlatList
-        data={pokemons?.results}
-        keyExtractor={pokemon => pokemon.name}
-        renderItem={PokemonShow}
-        style={{
-          marginHorizontal: 10,
-          marginVertical: 10,
-        }}
-        contentContainerStyle={{
-          flexGrow: 1,
-        }}
-      />
-    </View>
+    <>
+      {loading ? (
+        <View
+          style={{
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Loading />
+        </View>
+      ) : (
+        <View>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={pokemons?.results}
+            keyExtractor={pokemon =>
+              pokemon.url.replace(`${POKE_API}pokemon/`, '').replace('/', '')
+            }
+            renderItem={renderItem}
+            style={{
+              marginHorizontal: 10,
+              marginVertical: 10,
+            }}
+            contentContainerStyle={{
+              flexGrow: 1,
+            }}
+            onEndReached={loadMoreOnEnd}
+            onEndReachedThreshold={0.4}
+            ListFooterComponent={renderFooter}
+          />
+        </View>
+      )}
+    </>
   );
 };
 
